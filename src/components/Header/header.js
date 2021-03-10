ae061df7fa5395e1feb14e292568a83a736afab6
Nurdan Elemanov <e1eman1337@gmail.com>
2021-03-10 18:57:18 +0600

    hided burger menu from signin/signup pages

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 import './header.scss';
 import { useStore } from 'effector-react';
+import { useLocation } from 'react-router-dom';
 import { $isDark, changeMode } from '../../store/mode';
 import { BurgerMenu } from '../BurgerMenu/burgerMenu';
 
 export const Header = () => {
 
     const isDark = useStore($isDark);
+    const location = useLocation();
 
     useEffect(() => {
         localStorage.setItem('isDark', isDark);
     }, [isDark])
-
+    
     return(
         <header className={isDark ? 'header header_dark' : 'header'}>
             <div className="header__container">
-                <BurgerMenu />
+                {location.pathname === '/sign-in' | location.pathname === '/sign-up' ? <div />
+                :
+                <BurgerMenu />}
                 <div className={isDark ? 'header__mode header__mode_dark' : 'header__mode'} onClick={changeMode}>
                     <div />
                 </div>
